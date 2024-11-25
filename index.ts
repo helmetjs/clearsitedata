4740fbb6429b0f561a9c2bff351dbb16aebcef4a
Evan Hahn <me@evanhahn.com>
2024-11-25 03:40:29 +0000

    Use `import type` when importing from Node's HTTP module

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { IncomingMessage, ServerResponse } from "http";
+import type { IncomingMessage, ServerResponse } from "node:http";
 
 interface ClearSiteDataOptions {
   directives?: string[];
